81714b585f30bf8cbb08c7f6afeee0a3b620a867
conor.ney <conor.ux@gmail.com>
2022-08-24 00:46:53 -0400

    parsed csv data and found total wins

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import fs from 'fs';
 
-const matches = fs.readFileSync('football.csv', {
-  // if we don't specify the encoding, we'll return a
-  // buffer by default.
-  encoding: 'utf-8',
-});
+const matches = fs
+  .readFileSync('football.csv', {
+    // if we don't specify the encoding, we'll return a
+    // buffer by default.
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
 
-console.log(matches);
+let manUnitedWins: number = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
