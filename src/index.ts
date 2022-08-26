528838b409591625fa6230f17ac8ec44459d9b44
conor.ney <conor.ux@gmail.com>
2022-08-25 23:52:23 -0400

    string to date conversion utility

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
 // console.log(reader.data);
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
 
 let manUnitedWins: number = 0;
 
