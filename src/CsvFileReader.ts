3cdd9a8f19ba63a406ad2e4c8c046bd56f61356a
conor.ney <conor.ux@gmail.com>
2022-08-26 00:01:24 -0400

    CsvFileReader class works for futbol data

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string]
+
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
@@ -17,7 +20,7 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): any => {
+      .map((row: string[]): MatchData => {
         return [
           dateStringToDate(row[0]),
           row[1],
