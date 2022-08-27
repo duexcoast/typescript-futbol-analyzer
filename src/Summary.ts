e5dc3d51d7eb92e8e42edd33d13289b7f2bba21a
conor.ney <conor.ux@gmail.com>
2022-08-27 00:35:52 -0400

    buildAndReportMethod on Summary class

--- src/Summary.ts
+++ src/Summary.ts
@@ -9,5 +9,10 @@ export interface OutputTarget {
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public ouputTarget: OutputTarget) {}
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]) {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
