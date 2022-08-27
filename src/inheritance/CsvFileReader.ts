ee7d53fc6ca4a3d348c38fccc16e2e0cbce5275a
conor.ney <conor.ux@gmail.com>
2022-08-26 21:27:15 -0400

    refactored to use interfaces

--- src/inheritance/CsvFileReader.ts
+++ src/inheritance/CsvFileReader.ts
@@ -1,22 +1,27 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
+
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader<T> {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
+        // if we don't specify the encoding, we'll return a
+        // buffer by default.
         encoding: 'utf-8',
       })
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      });
+      })
+      .map(this.mapRow);
   }
 }
