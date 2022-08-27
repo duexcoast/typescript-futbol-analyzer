6e246539050bb644766025f4c3e877c511c3b695
conor.ney <conor.ux@gmail.com>
2022-08-27 00:23:38 -0400

    created Summary class. beginning refactor for composition

---
+++ src/Summary.ts
@@ -0,0 +1,13 @@
+import { MatchData } from './MatchData';
+
+interface Analyzer {
+  run(matches: MatchData[]): string;
+}
+
+interface OutputTarget {
+  print(report: string): void;
+}
+
+export class Summary {
+  constructor(public analyzer: Analyzer, public ouputTarget: OutputTarget) {}
+}
